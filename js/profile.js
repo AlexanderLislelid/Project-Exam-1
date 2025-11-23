5a836f1f40172352760d3af0bf9230d057234f2d
AlexanderLislelid <alexlislelid@gmail.com>
2025-11-23 16:59:26 +0100

    testing

--- js/profile.js
+++ js/profile.js
@@ -3,3 +3,22 @@ const token = localStorage.getItem("accessToken");
 if (!token) {
   window.location.href = "login.html";
 }
+
+// const profileWrapper = (document.getElementById = "profile-info");
+
+// const userName = document.createElement("h1");
+
+// const userData = localStorage.getItem("user");
+// let userNameText = "Unknown User";
+// if (userData) {
+//   try {
+//     const userObj = JSON.parse(userData);
+//     if (userObj && userObj.name) {
+//       userNameText = userObj.name;
+//     }
+//   } catch (e) {
+//     // handle JSON parse error, keep default
+//   }
+// }
+// userName.textContent = userNameText;
+profileWrapper.append(userName);
