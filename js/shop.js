df8873082496bd0e0d0dfc54a88b428606bec11f
AlexanderLislelid <alexlislelid@gmail.com>
2025-12-04 16:09:12 +0100

    Remove unused api.js file and add comment in shop.js for future updates

--- js/shop.js
+++ js/shop.js
@@ -2,3 +2,5 @@ import { fetchAllProducts } from "../js/product-feed.js";
 import { showLoader, hideLoader } from "./utils/loader.js";
 
 fetchAllProducts();
+
+// will be updated with updated functions such as search and categories at a later date
